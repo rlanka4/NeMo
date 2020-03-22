340eb2841f4716312bd93aef2e2bb1a62a0f1688
Stanislav Beliaev <stasbelyaev96@gmail.com>
2020-03-22 09:24:19 -0700

    Complete durs pipeline with evaluation.

--- nemo/core/callbacks.py
+++ nemo/core/callbacks.py
@@ -17,7 +17,9 @@
 
 import collections
 import glob
+import json
 import os
+import pprint
 import sys
 import time
 import warnings
@@ -634,27 +636,3 @@ class WandbCallback(ActionCallback):
         import wandb
 
         wandb.log(tensors_logged, step=self.step)
-
-
-class TrainLogger(SimpleLossLoggerCallback):
-    def __init__(self, tensors, metrics, freq, tb_writer, mu=0.99):
-        self._cache = collections.defaultdict(float)
-
-        def print_func(pt_tensors):
-            kv_tensors = attrdict.AttrDict(dict(zip(tensors.keys(), pt_tensors)))
-
-            for metric in metrics:
-                for k, v in metric(kv_tensors).items():
-                    self._cache[k] = (1 - mu) * self._cache[k] + mu * v
-
-        # noinspection PyUnusedLocal
-        def get_tb_values(*args, **kwargs):
-            return list((k, np.array(v)) for k, v in self._cache.items())
-
-        super().__init__(
-            tensors=list(tensors.values()),
-            print_func=print_func,
-            get_tb_values=get_tb_values,
-            step_freq=freq,
-            tb_writer=tb_writer,
-        )
